886c2be81532f500fceec69b423a2b9f35d5612f
Lakisha J <lakishajohnson@pursuit.org>
2023-10-30 20:50:46 -0400

    Landing Page Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from "react";
+
+import Landing from "./Components/Landing";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        {/* <NavBar /> */}
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          {/* <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} /> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
