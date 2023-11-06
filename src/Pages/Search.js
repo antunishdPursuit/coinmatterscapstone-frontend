5cd750fbcad9cbb0a19550bef7b91a3f89bade4c
Noni Porter <n@nonis-air.mynetworksettings.com>
2023-11-06 16:00:00 -0500

    css for coinMatters have been updating. Search page is done. Still need to work on Results page and Home page. I installed some packages for icons.

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -1,9 +1,18 @@
 import SearchBar from "../Components/Searches/SearchBar";
+import logo from "../Images/pig-background.png"
+import "../CSS/Search.css";
+
+
 
 export default function Search() {
     return (
-        <div className="search-results">
-            <SearchBar />
+        <div className="search-page-container">
+            <div>
+                <SearchBar />
+            </div>
+            <div className="background-img-container">
+                <img src={logo} alt="pig logo" />
+            </div>
         </div>
     );
 };
