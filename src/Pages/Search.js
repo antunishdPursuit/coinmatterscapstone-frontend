c03fca6156fb8d6b1c7f6f631ad2450340be4a20
Noni Porter <n@nonis-air.mynetworksettings.com>
2023-11-05 19:15:21 -0500

    important changes were made here 1. The CSS for the landing page was being applied to all of the pages due to the 'body' tag. I changed the body css marker to .landing-wrapper class - it is placed on the first div in the return for the Landing component (in landing.js file); 2. I commented out some code in SearchBar to make the mockdata work for CSS page styling for searh page and results page

---
+++ src/Pages/Search.js
@@ -0,0 +1,9 @@
+import SearchBar from "../Components/Searches/SearchBar";
+
+export default function Search() {
+    return (
+        <div className="search-results">
+            <SearchBar />
+        </div>
+    );
+};
